3979df20671f8b7545f1a7e4bd0c3d37d6e84c51
Dima Zeklam <dema.ziklam@gmail.com>
2021-12-11 14:42:53 +0200

    add config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "whattodo": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
