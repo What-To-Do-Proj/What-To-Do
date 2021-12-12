857d41ce602468441260b6afc3a8746cd20c2f0a
Dima Zeklam <dema.ziklam@gmail.com>
2021-12-12 02:06:31 +0200

    create register

---
+++ app/src/main/java/com/example/whattodo/Login.java
@@ -0,0 +1,60 @@
+package com.example.whattodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.amplifyframework.core.Amplify;
+
+public class Login extends AppCompatActivity {
+    private static final String TAG = "Login";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button LoginIn = findViewById(R.id.buttonLogin);
+//        EditText username = findViewById(R.id.LogUserName);
+        EditText email = findViewById(R.id.LogEmail);
+        EditText password = findViewById(R.id.LoginPassword);
+        Button createNewAccount = findViewById(R.id.newAccount);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor preferenceEditor = preferences.edit();
+
+        LoginIn.setOnClickListener(view -> {
+            LoginIn(email.getText().toString(), password.getText().toString());
+            preferenceEditor.putString("email",email.getText().toString());
+            preferenceEditor.apply();
+        });
+
+        createNewAccount.setOnClickListener(view -> {
+            Intent goToSignUp = new Intent(Login.this, Register.class);
+            startActivity(goToSignUp);
+        });
+
+
+
+    }
+
+    void LoginIn(String email, String password) {
+        Amplify.Auth.signIn(
+                email,
+                password,
+                success -> {
+                    Log.i(TAG, "signIn: worked " + success.toString());
+                    Intent goToMain = new Intent(Login.this, MainActivity.class);
+                    startActivity(goToMain);
+                },
+
+                error -> Log.e(TAG, "signIn: failed" + error.toString()));
+    }
+}
---
+++ app/src/main/java/com/example/whattodo/Register.java
@@ -0,0 +1,91 @@
+package com.example.whattodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+//import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+
+public class Register extends AppCompatActivity {
+    private static final String TAG = "Register";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        configureAmplify();
+
+
+        Button signUp = findViewById(R.id.btnReg);
+        EditText username = findViewById(R.id.RegUserName);
+        EditText email = findViewById(R.id.RegGmail);
+        EditText password = findViewById(R.id.RegPassword);
+         EditText phone = findViewById(R.id.RegPhone);
+        TextView signIn = findViewById(R.id.btnGotoLogin);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUp(
+                        username.getText().toString(),
+                        email.getText().toString(),
+                        password.getText().toString()
+                );
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(Register.this, Login.class);
+                startActivity(signIn);
+            }
+        });
+    }
+    void signUp(String username, String email, String password) {
+        Amplify.Auth.signUp(
+                email,
+                password,
+                AuthSignUpOptions.builder()
+                        .userAttribute(AuthUserAttributeKey.email(), email)
+                        .build(),
+                success -> {
+                    Log.i(TAG, "signUp successful: " + success.toString());
+                    Intent goToVerification = new Intent(Register.this, ConfirmActivity.class);
+                    goToVerification.putExtra("email", email);
+                    goToVerification.putExtra("password", password);
+                    startActivity(goToVerification);
+                },
+                error -> {
+                    Log.e(TAG, "signUp failed: " + error.toString());
+                });
+    }
+
+    private void configureAmplify() {
+        // configure Amplify plugins
+        try {
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            //need to install cognito
+//            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i(TAG, "Successfully initialized Amplify plugins");
+        } catch (AmplifyException exception) {
+            Log.e(TAG, "Failed to initialize Amplify plugins => " + exception.toString());
+        }
+
+    }
+}
